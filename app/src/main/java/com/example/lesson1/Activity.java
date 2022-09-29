7883539e09629b5d8b2889824723c1561edbe2d4
raz1boo <pavlik.shevchenko96@gmail.com>
2022-09-29 20:26:09 +0300

    Created file Lesson5

--- app/src/main/java/com/example/lesson1/Activity.java
+++ app/src/main/java/com/example/lesson1/Activity.java
@@ -2,8 +2,10 @@ package com.example.lesson1;
 
 public class Activity {
     public static void main(String[] args) {
+
         System.out.println("Hello");
         System.out.println("Hello2");
+
     }
 
 }
